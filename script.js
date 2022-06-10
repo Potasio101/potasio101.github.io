f015b904d77c580a6392501c59900d4330bbe5eb
Potasio101 <portsayco@gmail.com>
2022-06-09 20:46:43 -0700

    backupdate

---
+++ script.js
@@ -0,0 +1,13 @@
+
+/**
+Function is for call the objective page coming from the image uat.png
+*/
+function uatlandpage () {
+location.replace("objective.html"); // Objective page has the information of major objective
+}
+/**
+ * Function back is for go back to the landing page
+ */
+function back () {
+    location.replace("index.html"); // back button move you to the landing page
+    }
